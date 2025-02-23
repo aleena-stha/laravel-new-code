909497f55112126de998b0fb5467bc6124882dd4
aleena-stha <159105068+aleena-stha@users.noreply.github.com>
2025-02-23 20:47:30 +0545

    Add files via upload

---
+++ layout.blade.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+ <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title></title>
+</head>
+ <body>
+    @include('profile.partials.header')
+    @include('profile.partials.footer')
+  
+<section>
+    @yield('content')
+    @yield('content2')
+</section>
+
+        <!-- Fonts -->
+</body>
+</html>
+        
